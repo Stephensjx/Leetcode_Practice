5eaf4c42519bba8799e872d09e3193b9cd5645f2
Stephen Sun <stephensun@Stephen-Sun.fios-router.home>
2020-04-06 23:06:12 -0400

    2.Add_Two_Numbers_04062020

--- EVERY_SINGLE_DAY/Test.py
+++ EVERY_SINGLE_DAY/Test.py
@@ -9,3 +9,27 @@ def twoSum(nums, target):
 
 
 case = twoSum([2, 7, 11, 15], 9)
+
+#2.Add Two Numbers
+def addTwoNumbers(l1, l2):
+    """
+    :type l1: ListNode
+    :type l2: ListNode
+    :rtype: ListNode
+    """
+    head = ListNode(0)
+    l3 = head
+    carry = 0
+    while l1 or l2 or carry:
+        if l1:
+            carry += l1.val
+            l1 = l1.next
+        if l2:
+            carry += l2.val
+            l2 = l2.next
+        if carry:
+            l3.val = carry % 10
+            l3.next = ListNode(0)
+            l3 = l3.next
+        carry = carry // 10
+    return head
